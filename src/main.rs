ade7c60168b3ae0a274a4a6a804875b1abd00426
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-04 22:22:31 +0900

    add: 予想と秘密の数字を比較する

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
+
+    // --snip--
 
     let mut guess = String::new();
 
@@ -16,5 +19,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");                 //数値を入力してください！
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
